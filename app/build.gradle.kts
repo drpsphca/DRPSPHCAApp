987d2ebdddfaad7724e71d3150ec33e01cd75267
DRPS PHCA <contactdrpsphca@gmail.com>
2026-01-27 21:01:51 +0800

    20260127-03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx-android:2.10.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(project(":shared"))
 }
