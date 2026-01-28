40410ca125a3e8c46e00953fc15f11069da421a6
DRPS PHCA <contactdrpsphca@gmail.com>
2026-01-28 15:18:55 +0800

    20260128-02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "phcaapp-unstable-26.01.27.1"
+        versionName = "phcaapp-unstable-26.01.28.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
